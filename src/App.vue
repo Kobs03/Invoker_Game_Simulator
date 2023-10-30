ead8a042e791b40e1b72ba85c99bd81ef223be1b
Jake S. Dedicatoria <thisisjake03@gmail.com>
2023-10-31 01:02:32 +0800

    dist update

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
 <template>
   <h1>elo werld!</h1>
+  <h1>please work {{ message }}</h1>
 </template>
+
+<script>
+  export default {
+    data(){
+      return {
+        message:"ello world!"
+      }
+    }
+  }
+</script>
